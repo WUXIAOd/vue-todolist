e1981c6d913e4c5e39427c3919a5871797f3a8f2
wuxiao <1291383707@qq.com>
2020-03-05 17:05:05 +0800

    通过axios请求state 中的数据进行渲染

--- src/App.vue
+++ src/App.vue
@@ -29,26 +29,19 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'app',
   data() {
     return {
-      list: [
-        {
-          id: 0,
-          info: 'Racing car sprays burning fuel into crowd.',
-          done: false
-        },
-        { id: 1, info: 'Japanese princess to wed commoner.', done: false },
-        {
-          id: 2,
-          info: 'Australian walks 100km after outback crash.',
-          done: false
-        },
-        { id: 3, info: 'Man charged over missing wedding girl.', done: false },
-        { id: 4, info: 'Los Angeles battles huge wildfires.', done: false }
-      ]
     }
+  },
+  created() {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['list'])
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 // 2. 导入组件库的样式表
 import 'ant-design-vue/dist/antd.css'
+import store from '../src/store/index.js'
 
 Vue.config.productionTip = false
 // 3. 安装组件库
 Vue.use(Antd)
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount('#app')
-
