79654f35c4a4c87bb016fcedd50a840f0145cfb0
wuxiao <1291383707@qq.com>
2020-03-05 17:49:28 +0800

    添加选项功能初始

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <a-input placeholder="请输入任务" class="my_ipt" />
-    <a-button type="primary">添加事项</a-button>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputChange" />
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
 
     <a-list bordered :dataSource="list" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
@@ -41,7 +41,19 @@ export default {
     this.$store.dispatch('getList')
   },
   computed: {
-    ...mapState(['list'])
+    ...mapState(['list', 'inputValue'])
+  },
+  methods: {
+    // 监听文本框内容变化
+    handleInputChange(e) {
+      this.$store.commit('setInputValue', e.target.value)
+    },
+    addItemToList() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('输入内容不能为空！')
+      }
+      this.$store.commit('addItem')
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    list: []
+    list: [],
+    // 文本框内容
+    inputValue: '默认文本',
+    nextId: 5
   },
   mutations: {
     initList(state, list) {
       state.list = list
+    },
+    // 为store 中的inputValue赋值
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表项
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
     }
   },
   actions: {
